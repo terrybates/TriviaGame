cbe1b2cb83e5a437a8f57b9205fa751523d77a69
Theresa Bates <theresabates@Theresas-MacBook-Air.local>
2017-01-10 23:09:01 -0500

    adding new files

---
+++ assets/javascript/app.js
@@ -0,0 +1,32 @@
+//Time Clock Section
+$(document).ready(function() {
+    startClock();
+function startClock(){
+  function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.text(minutes + ":" + seconds);
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+jQuery(function ($) {
+    var twoMinutes = 60 * 2,
+        display = $('#time');
+    startTimer(twoMinutes, display);
+});
+}
+});
+
+
+                                
+            
